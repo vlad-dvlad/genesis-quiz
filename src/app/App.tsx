d806f57f64bd747992d0cd77293c8f263cede6ec
Vladyslav Ivashko <vlad.ivashko72@gmail.com>
2024-02-22 01:40:06 +0200

    Finish setup structures/fonts/i18next

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
 const App = () => {
+  const { t } = useTranslation();
   return (
     <div>
-      
+      {t('title1')}
     </div>
   );
 };
---
+++ src/i18n/setup.ts
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import backend from 'i18next-http-backend';
+import { LANGUAGES } from '../shared/config/common';
+
+i18n
+    .use(backend)
+    .use(initReactI18next)
+    .init({ 
+        backend: {
+            loadPath: '/locales/{{lng}}.json'
+        },
+        fallbackLng: 'en',
+        supportedLngs: LANGUAGES
+     }) 
+
+
+export default i18n;
+
