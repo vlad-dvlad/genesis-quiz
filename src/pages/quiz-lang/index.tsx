17a748f799c76683100fd47c166670d526b80546
Vladyslav Ivashko <vlad.ivashko72@gmail.com>
2024-02-22 22:07:13 +0200

    finish loader/progress

--- src/pages/quiz-lang/index.tsx
+++ src/pages/quiz-lang/index.tsx
@@ -1,12 +1,9 @@
-import Loader from "../../shared/ui/loader";
 import PageLayout from "../../shared/ui/page-layout/PageLayout";
-import Progress from "../../shared/ui/progress";
 
 const QuizLang = () => {
     return (
         <PageLayout>
-            <Progress step={3} total={5} />
-            <Loader />
+
         </PageLayout>
     );
 };
--- src/shared/ui/loader/index.tsx
+++ src/shared/ui/loader/index.tsx
@@ -1,14 +1,26 @@
+import { useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./styles.module.scss"
 
 const cx = classNames.bind(styles);
 
 const Loader = () => {
+    const [count, setCount] = useState(1);
+
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        if (count < 100) {
+          setCount(count + 1);
+        }
+      }, 45);
+  
+      return () => clearTimeout(timer);
+    }, [count]);
+    
     return (
-        <div className={cx("progress-bar-container")}>
-            <div className={cx("progress-bar html")}>
-                <progress className={cx("html")} max="100" value="92"></progress>
-            </div>
+        <div className={cx("wrapper")}>
+            <div className={cx("loader")} />
+            <span className={cx("loader__percent")}>{count} %</span>
         </div>
     );
 };
