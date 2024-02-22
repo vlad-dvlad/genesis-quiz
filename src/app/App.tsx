77098b92fb6f832d8c0b55a7cc2f9705bdc4ee8b
Vladyslav Ivashko <vlad.ivashko72@gmail.com>
2024-02-22 12:18:12 +0200

    finish lang setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,17 @@
-import React from "react";
 import { useTranslation } from "react-i18next";
+import { LANGUAGES } from "../shared/config/common";
 
 const App = () => {
-  const { t } = useTranslation();
+  const { t, i18n  } = useTranslation();
+  
   return (
     <div>
       {t('title1')}
+      {
+        LANGUAGES.map((lang) => (
+          <div key={lang} onClick={() => i18n.changeLanguage(lang)}>{lang}</div>
+        ))
+      }
     </div>
   );
 };
