241416fe8bfb9d8c84f67ec53c617d113a275cf8
Vladyslav Ivashko <vlad.ivashko72@gmail.com>
2024-02-23 12:41:01 +0200

    book hate base structure

--- src/pages/quiz-book-hate/index.tsx
+++ src/pages/quiz-book-hate/index.tsx
@@ -1,12 +1,18 @@
 import PageLayout from "../../shared/ui/page-layout/PageLayout";
+import { useTranslation } from "react-i18next";
+import classNames from "classnames/bind";
+import styles from "./styles.module.scss"
+
+const cx = classNames.bind(styles);
 
 const QuizBookHate = () => {
+    const { t } = useTranslation()
+
     return (
         <PageLayout>
-        <div style={{ color: '#eee' }}>
-        xzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-        </div>
-    </PageLayout>
+            <div className={cx('gender__title')}>{t('title4')}</div>
+            
+        </PageLayout>
     );
 };
 
